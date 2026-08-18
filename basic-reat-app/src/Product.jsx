46588ca8157b10614f5dd74bb66bb8a47dc0deb8
Md Fahim <217421678+mdfahim10@users.noreply.github.com>
2026-08-18 06:54:38 +0530

    Passing and rendering arrays as props

--- basic-reat-app/src/Product.jsx
+++ basic-reat-app/src/Product.jsx
@@ -1,10 +1,16 @@
 import "./Product.css"
 function Product ({title,price,Description}){
+    const features = ["Fast", "Powerful", "Lightweight"];
     return (
         <div className="product">
             <h3>{title}</h3>
             <p>price : {price}</p>
             <h5>{Description}</h5>
+            <h4>Features : </h4>
+            <ul>
+                {features.map((feature)=>{
+                    return <li>{feature}</li>})}
+            </ul>
         </div>
     );
 }
