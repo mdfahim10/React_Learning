720e42168d04451f4d3b7a3a87f60480a39b3757
Md Fahim <217421678+mdfahim10@users.noreply.github.com>
2026-08-21 07:12:47 +0530

    React forms -- Single input

---
+++ react-form-/src/Form.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+
+export default function Form() {
+
+    let [fullName, setFullName] = useState("");
+    let handleNameChange = (event) => {
+        setFullName(event.target.value)
+    };
+    return (
+        <form>
+            <input
+                placeholder="enter full name"
+                type="text"
+                value={fullName}
+                onChange={handleNameChange}
+            />
+
+            <br></br>
+
+            <button>Submit</button>
+        </form>
+    )
+}
