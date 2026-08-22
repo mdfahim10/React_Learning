1f0a8f75eb04f1ab0237636546876dcf1bf585af
Md Fahim <217421678+mdfahim10@users.noreply.github.com>
2026-08-22 12:31:21 +0530

    Multiple Input Form

--- react-form-/src/App.jsx
+++ react-form-/src/App.jsx
@@ -6,9 +6,7 @@ import Form from "./Form"
 function App() {
 
   return (
-    <div>
     <Form />
-    </div>
   )
 }
 
--- react-form-/src/Form.jsx
+++ react-form-/src/Form.jsx
@@ -1,23 +1,70 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export default function Form() {
 
+    /*  Sinngle Input
     let [fullName, setFullName] = useState("");
     let handleNameChange = (event) => {
         setFullName(event.target.value)
     };
+    */
+
+    
+    // Multiple Input
+    let [formData, setFormData] = useState({
+        fullName: "",
+        username: ""
+    });
+
+    let handleInputChange = (event) => {
+        setFormData((currData) => {
+            return {
+                ...currData,
+                [event.target.name]: event.target.value
+            };
+        });
+    };
+
+    let handleSubmit = (event) => {
+        event.preventDefault();
+
+        setFormData({
+            fullName: "",
+            username: ""
+        });
+    };
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
+
+            <label htmlFor="fullName">Full Name</label>
+
             <input
-                placeholder="enter full name"
+                placeholder="Enter full name"
                 type="text"
-                value={fullName}
-                onChange={handleNameChange}
+                value={formData.fullName}
+                id="fullName"
+                name="fullName"
+                onChange={handleInputChange}
             />
 
-            <br></br>
+            <br />
 
-            <button>Submit</button>
+            <label htmlFor="username">Username</label>
+
+            <input
+                placeholder="Enter user name"
+                type="text"
+                value={formData.username}
+                id="username"
+                name="username"
+                onChange={handleInputChange}
+            />
+
+            <br />
+
+            <button type="submit">Submit</button>
+
         </form>
-    )
+    );
 }
